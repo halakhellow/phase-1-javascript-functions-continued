f94b5793743617cfcfa32d99eb54057e9bc33675
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-11 22:42:09 +0300

    add function expression wrapAdjective which returns another function (closure)

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function saturdayFun(activity="roller-skate"){
 let mondayWork = function(activity="go to the office") {
     return `This Monday, I will ${activity}.`
    }
- 
+   
+let wrapAdjective = function(wrapper="*"){
+    return function(adj="special"){
+        return `You are ${wrapper}${adj}${wrapper}!`
+    }
+}
